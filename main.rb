819c7601947686be6b24c716fb8a50605fe6d8b6
Aleksei Nizhnik <nizhnik.aleksei@gmail.com>
2016-02-07 23:46:11 +0200

    Task_2

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
+require 'pry'
 
+x_point, y_point, x, y = ARGV
+
+if x_point == x and y_point == y
+  puts 'Точка найдена!'
+elsif x_point == x
+  puts 'х координата верна, y нет'
+elsif y_point == y
+  puts 'y координата верна, x нет'
+else 
+  puts 'Близко, но нет'
+end
